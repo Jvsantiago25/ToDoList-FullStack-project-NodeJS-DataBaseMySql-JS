992ffa92e654d7dae022223ca10dbc76d219f182
Jvsantiago25 <joaovictor.santiagorm5@gmail.com>
2023-10-30 07:12:54 -0300

    ToDoList-FullStack-Project

---
+++ backend/src/router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const router = express.Router();
+
+const tasksController = require("./controllers/tasksController");
+const tasksMiddleware = require("./middlewares/tasksMiddleware");
+const { deleteTask } = require("./models/tasksModel");
+
+router.get("/tasks", tasksController.getAll);
+router.post(
+  "/tasks",
+  tasksMiddleware.validateFieldTitle,
+  tasksController.createTask
+);
+router.delete("/tasks/:id", tasksController.deleteTask);
+router.put(
+  "/tasks/:id",
+  tasksMiddleware.validateFieldTitle,
+  tasksMiddleware.validateFieldStatus,
+  tasksController.updateTask
+);
+
+module.exports = router;
